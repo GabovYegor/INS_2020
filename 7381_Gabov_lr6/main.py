99a0daeec1d7159ee7a7ae8a57dc15c139fb806e
Egor Gabov <gabov.egor2017@yandex.ru>
2020-05-05 13:36:24 +0300

    added lr6

---
+++ 7381_Gabov_lr6/main.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import models
+
+from keras import layers
+from keras.datasets import imdb
+
+dim = 500
+(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=dim)
+data = np.concatenate((training_data, testing_data), axis=0)
+targets = np.concatenate((training_targets, testing_targets), axis=0)
+
+def check_result(result):
+    if result == 1:
+        print("Positive review")
+    else:
+        print("Negative review")
+
+
+def vectorize(sequences, dimension=dim):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+def draw_plot(type):
+    plt.clf()
+    if (type == "loss"):
+        plt.plot(results.history['loss'])
+        plt.plot(results.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+    elif (type == "acc"):
+        plt.plot(results.history['accuracy'])
+        plt.plot(results.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def custom_review(name):
+    f_text = open(name, 'r').read()
+    word_index = imdb.get_word_index()
+    text = []
+    for i in f_text:
+        if i in word_index and word_index[i] < dim:
+            text.append(word_index[i])
+
+    text = vectorize([text])
+    return text
+
+
+data = vectorize(data)
+targets = np.array(targets).astype("float32")
+
+test_x = data[:10000]
+test_y = targets[:10000]
+train_x = data[10000:]
+train_y = targets[10000:]
+
+model = models.Sequential()
+model.add(layers.Dense(50, activation="relu", input_shape=(dim,)))
+
+model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
+model.add(layers.Dense(50, activation="relu"))
+model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
+model.add(layers.Dense(50, activation="relu"))
+
+model.add(layers.Dense(1, activation="sigmoid"))
+model.summary()
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+results = model.fit(train_x, train_y, epochs=2, batch_size=500, validation_data=(test_x, test_y))
+print(np.mean(results.history["val_accuracy"]))
+
+print("Testing positive:")
+check_result(model.predict_classes(custom_review("pos.txt")))
+
+print("Testing negative:")
+check_result(model.predict_classes(custom_review("neg.txt")))
+
+draw_plot("loss")
+draw_plot("acc")
