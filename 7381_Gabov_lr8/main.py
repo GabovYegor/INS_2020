771bf393a6afd17a90b872397e99c0a8ef81f58f
Egor Gabov <gabov.egor2017@yandex.ru>
2020-05-16 17:33:03 +0300

    update

--- 7381_Gabov_lr8/main.py
+++ 7381_Gabov_lr8/main.py
@@ -8,17 +8,37 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
-class GenCallback(tensorflow.keras.callbacks.Callback):
+def build_model():
+    model = Sequential()
+    model.add(LSTM(250, input_shape=(X.shape[1], X.shape[2])))
+    model.add(Dropout(0.25))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    return model
+
+def generateSequence(model):
+    start = numpy.random.randint(0, len(dataX)-1)
+    pattern = dataX[start]
+    print("Result:")
+    print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+    for i in range(1000):
+        x = numpy.reshape(pattern, (1, len(pattern), 1))
+        x = x / float(n_vocab)
+        prediction = model.predict(x, verbose=0)
+        index = numpy.argmax(prediction)
+        result = int_to_char[index]
+        sys.stdout.write(result)
+        pattern.append(index)
+        pattern = pattern[1:len(pattern)]
+
+class Callback(tensorflow.keras.callbacks.Callback):
     def __init__(self, epochs): 
-        super(GenCallback, self).__init__()
+        super(Callback, self).__init__()
         self.epochs = epochs
 
     def on_epoch_end(self, epoch, logs={}):
         if epoch in self.epochs:
             generateSequence(model)
 
-
-#загрузка текста ASCII в память и преобразование всех символов в нижний регистр
 filename = "wonderland.txt"
 raw_text = open(filename).read()
 raw_text = raw_text.lower()
@@ -27,13 +47,11 @@ chars = sorted(list(set(raw_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
 int_to_char = dict((i, c) for i, c in enumerate(chars))
 
-#суммирование набора данных
 n_chars = len(raw_text)
 n_vocab = len(chars)
 print("Total Characters: ", n_chars)
 print("Total Vocab: ", n_vocab)
 
-#разделение книги на последовательности
 seq_length = 100
 dataX = []
 dataY = []
@@ -43,49 +61,13 @@ for i in range(0, n_chars - seq_length, 1):
         dataX.append([char_to_int[char] for char in seq_in])
         dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
-print("Total Patterns: ", n_patterns)
+print("Patterns: ", n_patterns)
 
-# reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-
-# normalize
-X = X / float(n_vocab)
-
-# one hot encode the output variable
-y = np_utils.to_categorical(dataY)
-
-
-def generateSequence(model):
-# pick a random seed
-    start = numpy.random.randint(0, len(dataX)-1)
-    pattern = dataX[start]
-    print("Seed:")
-    print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
-    for i in range(1000):
-        x = numpy.reshape(pattern, (1, len(pattern), 1))
-        x = x / float(n_vocab)
-        prediction = model.predict(x, verbose=0)
-        index = numpy.argmax(prediction)
-        result = int_to_char[index]
-        seq_in = [int_to_char[value] for value in pattern]
-        sys.stdout.write(result)
-        pattern.append(index)
-        pattern = pattern[1:len(pattern)]
-
-
-# define the LSTM model
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
-model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
-
+model = build_model()
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
+path= "weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(path, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks = [checkpoint, Callback([0, 5, 10, 15])]
 
-# define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint, GenCallback([0, 5, 10, 15, 19])]
-
-
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+model.fit(numpy.reshape(dataX, (n_patterns, seq_length, 1)) / float(n_vocab), np_utils.to_categorical(dataY), epochs=15, batch_size=100, callbacks=callbacks)
